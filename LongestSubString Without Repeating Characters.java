3253bbc059960ee3b032a771ad4e9319710caddd
Sourish Mukherjee <56296940+Sourish-Mukherjee@users.noreply.github.com>
2020-04-27 13:08:53 +0530

    Added Comments to the code for better understanding.

--- LongestSubString Without Repeating Characters.java
+++ LongestSubString Without Repeating Characters.java
@@ -1,13 +1,22 @@
+/**
+ Author : Sourish Mukherjee
+ Language: Java
+ Date : 27-04-2020
+*/
+
 class Solution {
     public int lengthOfLongestSubstring(String s) {
+        //Use Of HashSet, because Sets don't allow duplicate characters
         HashSet<Character> set = new HashSet();
+        //Use Of Two Pointers left and right
         int left = 0,right =0,max = 0,n = s.length();
         while(right<n) {
+            //If the set does not contain the given character, add it to the set
             if(!set.contains(s.charAt(right)))
-                set.add(s.charAt(right++));
+                set.add(s.charAt(right++));// Post increment the right operator
             else
-                set.remove(s.charAt(left++));   
-            max = Math.max(set.size(),max);
+                set.remove(s.charAt(left++));// If it does contain the set, remove the characters from the left
+            max = Math.max(set.size(),max);//Cacluclate max size by checking set size and max value
         }
         return max;
     }
