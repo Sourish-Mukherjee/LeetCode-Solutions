1c1806096b325322d0a4ed260beec973067b2e38
Sourish Mukherjee <56296940+Sourish-Mukherjee@users.noreply.github.com>
2020-05-13 18:13:57 +0530

    Create Divisor Game.java

---
+++ Divisor Game.java
@@ -0,0 +1,10 @@
+/*
+  Author: Sourish Mukherjee
+  Date: 13-05-2020
+  Link: https://leetcode.com/problems/divisor-game/
+*/
+class Solution {
+    public boolean divisorGame(int N) {
+        return N%2==0;
+    }
+}
