8a63d18d85a875d88e37e86e4643e7ecec99237c
Sourish Mukherjee <56296940+Sourish-Mukherjee@users.noreply.github.com>
2020-05-19 20:53:33 +0530

    Create Find N Unique Integers Sum up to Zero.java

---
+++ Find N Unique Integers Sum up to Zero.java
@@ -0,0 +1,11 @@
+//Link: https://leetcode.com/problems/find-n-unique-integers-sum-up-to-zero/
+
+class Solution {
+    public int[] sumZero(int n) {
+        int[] ans = new int[n];
+        for(int i=0; i<n; i++) {
+            ans[i]  = (i*2)-n+1;
+        }
+        return ans;
+    }
+}
