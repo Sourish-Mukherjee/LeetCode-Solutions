2380c34a1f17d58b68223ae3d3afd50bb61e319f
Sourish Mukherjee <56296940+Sourish-Mukherjee@users.noreply.github.com>
2020-07-18 10:53:01 +0530

    Create Binary Tree Pruning.java

---
+++ Binary Tree Pruning.java
@@ -0,0 +1,12 @@
+//Link: https://leetcode.com/problems/binary-tree-pruning/
+
+class Solution {
+        public TreeNode pruneTree(TreeNode root) {
+            if (root == null) return null;
+            root.left = pruneTree(root.left);
+            root.right = pruneTree(root.right);
+            if (root.val == 0 && root.left == null && root.right == null) root = null;
+            return root;
+   
+        }
+}
